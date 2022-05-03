83f625ceebe7c3b07656f1e786dd66bf5930559c
Harshit <2105891@kiit.ac.in>
2022-05-03 11:18:04 +0530

    strated off with resturant wbesite

---
+++ Resturant website/index.js
@@ -0,0 +1,15 @@
+var food= document.getElementsByClassName("foodName");
+var hotel= document.getElementsByClassName("hotelName");
+var iamge= document.getElementById("FoodImage");
+var prev= document.getElementById("prev");
+var random= document.getElementById("random");
+var next= document.getElementById("next");
+
+const menuItems=[
+    {
+        id:1,
+        food:"food item 1",
+        hotel:"hotel 1",
+        img="img/food1.jpg"
+    }
+]
